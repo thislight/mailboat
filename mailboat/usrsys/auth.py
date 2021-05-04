820870c7346e0a8e0da5fab26202639b0cb37776
thisLight <l1589002388@gmail.com>
2021-05-04 21:52:35 +0800

    AuthProvider.auth: requesting token

--- mailboat/usrsys/auth.py
+++ mailboat/usrsys/auth.py
@@ -14,14 +14,18 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Mailboat.  If not, see <http://www.gnu.org/licenses/>.
-from mailboat.usrsys.storage import UserRecordStorage
+from mailboat.usrsys.storage import TokenRecordStorage, UserRecordStorage
 from typing import List, Optional
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass
 class AuthRequest(object):
     username: Optional[str]
     password: Optional[str] # TODO (rubicon): use a customised type to prevent it being logged
+    token: Optional[str]
+    appid: Optional[str]
+    new_token_scope: List[str] = field(default_factory=list)
+    request_token: bool = False
 
 
 @dataclass
@@ -30,18 +34,25 @@ class AuthAnswer(object):
     success: bool
     required_second_factors: List[str]
     scope: List[str] # Note: currently it does not being used
+    token: Optional[str] = None
 
 
 class AuthProvider(object):
-    def __init__(self, user_record_storage: UserRecordStorage) -> None:
+    def __init__(self, user_record_storage: UserRecordStorage, token_record_storage: TokenRecordStorage) -> None:
         self.user_record_storage = user_record_storage
+        self.token_record_storage = token_record_storage
         super().__init__()
 
     async def auth(self, request: AuthRequest) -> AuthAnswer:
         if request.username and request.password:
             password_checking = await self.user_record_storage.check_user_password(request.username, request.password.encode('utf-8'))
             if password_checking:
-                return AuthAnswer(handled=True, success=True, required_second_factors=[], scope=[])
+                token = None
+                if request.request_token:
+                    user_record = await self.user_record_storage.find_one({'username': request.username})
+                    if user_record:
+                        token = await self.token_record_storage.create_token(user_record.profileid, appid=request.appid, scope=request.new_token_scope) # TODO (rubicon): set apprev from latest app config (after app things done)
+                return AuthAnswer(handled=True, success=True, required_second_factors=[], scope=[], token=(token.token if token else None))
             else:
                 return AuthAnswer(handled=True, success=False, required_second_factors=[], scope=[])
         else:
