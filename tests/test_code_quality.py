6627cacf83735c164ce82779d2a25bf0e9b06a23
thisLight <l1589002388@gmail.com>
2021-05-06 21:32:53 +0800

    add code quality test

---
+++ tests/test_code_quality.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2021 The Mailboat Contributors
+#
+# This file is part of Mailboat.
+#
+# Mailboat is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mailboat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mailboat.  If not, see <http://www.gnu.org/licenses/>.
+import subprocess
+
+
+def test_code_is_formatted():
+    result = subprocess.run(
+        ("poetry", "run", "black", "--check", "mailboat/", "tests/")
+    )
+    assert result.returncode == 0, "the code should be formatted"
