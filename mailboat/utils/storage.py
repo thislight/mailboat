444b30de0030e41d1492b9dfbeaff679af9ddb0e
thisLight <l1589002388@gmail.com>
2021-05-07 09:02:39 +0800

    DataclassCommonStorageAdapter: remove unexpected __id before pass into dataclass constructor

--- mailboat/utils/storage.py
+++ mailboat/utils/storage.py
@@ -113,6 +113,9 @@ class DataclassCommonStorageAdapter(Generic[T], CommonStorageAdapter[T]):
         super().__init__()
 
     def dict2record(self, d: Dict[str, Any]) -> T:
+        d = d.copy()
+        if "__id" in d:
+            d.pop("__id")
         return self.datacls(**d)  # type: ignore # it should work
 
     def record2dict(self, record: T) -> Dict[str, Any]:
